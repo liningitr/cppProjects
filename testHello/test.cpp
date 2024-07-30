848068e870f430680392eb116c46a3aeb56eb912
Ning Li <nli98@its.jnj.com>
2024-07-29 23:27:10 -0700

    first commit to develop branch

---
+++ testHello/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, sum;
+    num1 = 10;
+    num2 = 20;
+    sum = num1 + num2;
+    
+    cout << "Hello World!";
+
+    return 0;
+}
